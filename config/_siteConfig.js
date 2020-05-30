c3dc431a395500628cdb25ec592c72a9bf21b8e0
Eduardo Beltrame <munfred@brandeis.edu>
2020-05-30 18:10:58 -0300

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -39,8 +39,8 @@ export default {
 
   // Card Layout
   cards: {
-    imageDimensions: '2x1', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
-    theme: 'boxes' // Options: boxes, grid, image-grid
+    imageDimensions: '3x2', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
+    theme: 'grid' // Options: boxes, grid, image-grid
   },
 
   // Disqus
@@ -52,7 +52,7 @@ export default {
 
   // Newsletter Subscribe
   newsletter: {
-    on: false,
+    on: true,
     heading: 'Subscribe to Our Newsletter',
     btnText: 'Subscribe',
     // Can be the form action on a mail chimp form, a hubspot form,

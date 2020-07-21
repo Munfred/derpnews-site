1cc15ce6affad2b945c6e86f59602d9c3137c53f
Dylan Bannon <dylanbannon5@gmail.com>
2020-07-20 17:56:17 -0700

    not working yet

---
+++ components/grids/FeaturedPostsGrid.vue
@@ -0,0 +1,37 @@
+<template>
+  <featured-resource-grid
+    v-bind="$props"
+    :resource="$cms.post"
+    :theme="$siteConfig.posts.theme || $siteConfig.cards.theme || null"
+  >
+    <template v-slot:default="{ item }">
+      <post-card
+        :title="item.title"
+        :link="item.slug ? `/${item.slug}` : ''"
+        :image="item.featureImage"
+        :author="item.author"
+        :date="item.date"
+      />
+    </template>
+  </featured-resource-grid>
+</template>
+
+<script>
+import PostCard from '~/components/cards/PostCard'
+export default {
+  name: 'PostsGrid',
+  components: { PostCard },
+  props: {
+    perRow: { type: Number, default: 5 },
+    number: { type: Number, default: 0 },
+    order: { type: String, default: 'DESC' },
+    category: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    exclude: { type: String, default: '' }
+  }
+}
+</script>
